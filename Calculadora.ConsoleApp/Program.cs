ab3e0203e7558edd028d003bda2d50665e63c201
RavoalM <alvarofeltrin2006@uniplaclages.edu.br>
2025-03-10 14:41:09 -0300

    Atualização no code e adição de loop

--- Calculadora.ConsoleApp/Program.cs
+++ Calculadora.ConsoleApp/Program.cs
@@ -5,130 +5,127 @@ namespace Calculadora.ConsoleApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Calculadora Tabajara 2025");
 
-            //menu
-            Console.WriteLine("Escolha uma das opções:");
-            Console.WriteLine("1.Soma");
-            Console.WriteLine("2.Subtração");
-            Console.WriteLine("3.Multiplicação");
-            Console.WriteLine("4.Divisão");
-            Console.WriteLine("E.Sair");
-
-            String opcao = Console.ReadLine();
-            String opcaoValida = opcao.ToUpper();//ToUpper converte a string para maiúscula
-
-            //Sair
-            if (opcaoValida == "E")
+            while (true)
             {
-                return;
-            }
+                Console.WriteLine("Calculadora Tabajara 2025");
+
+                //menu
+                Console.WriteLine("1.Soma");
+                Console.WriteLine("2.Subtração");
+                Console.WriteLine("3.Multiplicação");
+                Console.WriteLine("4.Divisão");
+                Console.WriteLine("E.Sair");
+
+                Console.WriteLine();
+                Console.Write("Escolha uma opção: ");
+
+                String opcao = Console.ReadLine();
+                String opcaoValida = opcao.ToUpper();//ToUpper converte a string para maiúscula
 
 
-            //Soma
-            else if (opcaoValida == "1")
-            {
 
-                //coleta de dados
+                //Sair
+                if (opcaoValida == "E")
+                {
+                    break;
+                }
 
-                Console.Write("\nPor favor digite o primeiro número: ");
-                double n1 = Convert.ToDouble(Console.ReadLine());
-                Console.Write("Por favor digite o segundo número: ");
-                double n2 = Convert.ToDouble(Console.ReadLine());
+                Console.Write("Digite o primeiro número: ");
+                double n1s = Console.ReadLine();
+                double n1 = Convert.ToInt32(n1s);
 
+                Console.Write("Digite o segundo número: ");
+                string n2s = Console.ReadLine();
+                double n2 = Convert.ToInt32(n2s);
 
-                //processo
-                double resultado = n1 + n2;
+                double resultado = 0;
 
-                //saida
-                Console.WriteLine("\n=======================================");
-                Console.WriteLine("O primeiro número é: " + n1);
-                Console.WriteLine("O segundo número é: " + n2);
-                Console.WriteLine("Resultado da soma: " + resultado);
-                Console.WriteLine("=======================================");
+                //Soma
+                else if (opcaoValida == "1")
+                {
 
-                Console.ReadLine();
-            }
+                    //processo
+                    resultado = n1 + n2;
 
-
-            //Subtração
-            else if (opcaoValida == "2")
-            {
-
-                //coleta de dados
-                Console.Write("\nPor favor digite o primeiro número: ");
-                double n1 = Convert.ToDouble(Console.ReadLine());
-                Console.Write("Por favor digite o segundo número: ");
-                double n2 = Convert.ToDouble(Console.ReadLine());
+                    //saida
+                    Console.WriteLine("\n=======================================");
+                    Console.WriteLine("O primeiro número é: " + n1);
+                    Console.WriteLine("O segundo número é: " + n2);
+                    Console.WriteLine("Resultado da soma: " + resultado);
+                    Console.WriteLine("=======================================");
+                    Console.WriteLine("Deseja continuar (S/N): ");
 
 
-                //processo
-                double resultado = n1 - n2;
-
-                //saida
-                Console.WriteLine("\n=======================================");
-                Console.WriteLine("O primeiro número é: " + n1);
-                Console.WriteLine("O segundo número é: " + n2);
-                Console.WriteLine("Resultado da sua subtração é: " + resultado);
-                Console.WriteLine("=======================================");
-
-                Console.ReadLine();
-            }
+                    Console.ReadLine();
+                }
 
 
-            //Multiplicação
-            else if (opcaoValida == "3")
-            {
+                //Subtração
+                else if (opcaoValida == "2")
+                {
 
-                //coleta de dados
-                Console.Write("\nPor favor digite o primeiro número: ");
-                double n1 = Convert.ToDouble(Console.ReadLine());
-                Console.Write("Por favor digite o segundo número: ");
-                double n2 = Convert.ToDouble(Console.ReadLine());
+                    //processo
+                    resultado = n1 - n2;
 
+                    //saida
+                    Console.WriteLine("\n=======================================");
+                    Console.WriteLine("O primeiro número é: " + n1);
+                    Console.WriteLine("O segundo número é: " + n2); 
+                    Console.WriteLine("Resultado da sua subtração é: " + resultado);
+                    Console.WriteLine("=======================================");
+                    Console.WriteLine("Deseja continuar (S/N): ");
 
-                //processo
-                double resultado = n1 * n2;
-
-                //saida
-                Console.WriteLine("\n=======================================");
-                Console.WriteLine("O primeiro número é: " + n1);
-                Console.WriteLine("O segundo número é: " + n2);
-                Console.WriteLine("Resultado da sua multiplacação é: " + resultado);
-                Console.WriteLine("=======================================");
-
-                Console.ReadLine();
-            }
+                    Console.ReadLine();
+                }
 
 
-            //Divisão
-            else if (opcaoValida == "4")
-            {
+                //Multiplicação
+                else if (opcaoValida == "3")
+                {
 
-                //coleta de dados
-                Console.Write("\nPor favor digite o primeiro número: ");
-                double n1 = Convert.ToDouble(Console.ReadLine());
-                Console.Write("Por favor digite o segundo número: ");
-                double n2 = Convert.ToDouble(Console.ReadLine());
+                    //processo
+                    resultado = n1 * n2;
 
+                    //saida
+                    Console.WriteLine("\n=======================================");
+                    Console.WriteLine("O primeiro número é: " + n1);
+                    Console.WriteLine("O segundo número é: " + n2);
+                    Console.WriteLine("Resultado da sua multiplacação é: " + resultado);
+                    Console.WriteLine("=======================================");
+                    Console.WriteLine("Deseja continuar (S/N): ");
 
-                //processo
-                double resultado = n1 * n2;
-
-                //saida
-                Console.WriteLine("\n=======================================");
-                Console.WriteLine("O primeiro número é: " + n1);
-                Console.WriteLine("O segundo número é: " + n2);
-                Console.WriteLine("Resultado da sua divisão é: " + resultado);
-                Console.WriteLine("=======================================");
-
-                Console.ReadLine();
-            }
+                    Console.ReadLine();
+                }
 
 
-            else if (opcaoValida == "")
-            {
-                Console.WriteLine("Opção inválida, por favor escolha uma opção válida");
+                //Divisão
+                else if (opcaoValida == "4")
+                {
+
+                    //processo
+                    resultado = n1 / n2;
+
+                    //saida
+                    Console.WriteLine("\n=======================================");
+                    Console.WriteLine("O primeiro número é: " + n1);
+                    Console.WriteLine("O segundo número é: " + n2);
+                    Console.WriteLine("Resultado da sua divisão é: " + resultado);
+                    Console.WriteLine("=======================================");
+                    Console.WriteLine("Deseja continuar (S/N): ");
+
+                    Console.ReadLine();
+                }
+
+
+                else if (opcaoValida == "")
+                {
+                    Console.WriteLine("Opção inválida, por favor escolha uma opção válida");
+
+                    break;
+                }
+
+            
             }
         }
     }
